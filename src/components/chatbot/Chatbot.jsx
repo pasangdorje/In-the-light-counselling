33c60a669ce58eadc32c7bbfe31fa29722fed3a0
Pasang Dorje Lama <pasangdorje.lama0930@gmail.com>
2024-08-29 22:25:32 +1000

    Chatbot component edited

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -1,34 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 const Chatbot = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
   useEffect(() => {
-    if (!document.getElementById('botpress-script')) {
-      const botScript = document.createElement('script');
-      botScript.id = 'botpress-script';
-      botScript.src = 'https://cdn.botpress.cloud/webchat/v2.1/inject.js';
-      botScript.async = true;
+    // Load the Botpress webchat scripts
+    const script1 = document.createElement('script');
+    script1.src = 'https://cdn.botpress.cloud/webchat/v2.1/inject.js';
+    script1.async = true;
 
-      document.body.appendChild(botScript);
+    const script2 = document.createElement('script');
+    script2.src = 'https://mediafiles.botpress.cloud/c9684d82-1f66-4f98-a908-c5ba6f15f313/webchat/v2.1/config.js';
+    script2.async = true;
 
-      botScript.onload = () => {
-        setIsLoaded(true); 
-      };
-    } else {
-      setIsLoaded(true); 
-    }
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
 
+    // Cleanup function to remove scripts when component unmounts
     return () => {
-      // Cleanup if needed
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
     };
   }, []);
 
   return (
     <div>
-      {isLoaded && (
-        <div id="bp-web-widget" />
-      )}
+      <div id="bp-web-widget" />
     </div>
   );
 };
